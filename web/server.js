6f9ebdc05ea4344212725bd379bcb40924cb5a07
yamadapc <tacla.yamada@gmail.com>
2015-01-12 14:55:03 -0200

    Make our development server very slightly smarter
    It now uses a variable to store the port and logs when it starts up.

--- web/server.js
+++ web/server.js
@@ -1,5 +1,10 @@
 'use strict';
 var express = require('express');
 var app = express();
+
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
-app.listen(3000);
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
